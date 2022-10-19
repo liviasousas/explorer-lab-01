bac0f079c44172ce6c5b30edeed8556c07a7ec5f
liviasousas <sousalivia119@gmail.com>
2022-10-19 00:55:49 -0300

    cartao

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
 import "./css/index.css"
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-chill(2) path")
+ccBgColor01.setAttribute("fill", "green")
+ccBgColor02.setAttribute("fill", "purple")
